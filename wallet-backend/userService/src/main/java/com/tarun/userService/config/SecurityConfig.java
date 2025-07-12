d90d50e4922b1ce2ba09e488b5881873d96602fc
Tarun27 <tarunkr27@gmail.com>
2025-07-12 23:24:12 +0530

    refactored folder structure, have put folder for frontend code

--- wallet-backend/userService/src/main/java/com/tarun/userService/config/SecurityConfig.java
+++ wallet-backend/userService/src/main/java/com/tarun/userService/config/SecurityConfig.java
@@ -27,7 +27,8 @@ public class SecurityConfig {
                                 "/users/login",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html",
+                                "/actuator/prometheus"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
